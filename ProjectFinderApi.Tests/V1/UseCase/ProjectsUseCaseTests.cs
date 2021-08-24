361a9d4155bedb7879d9e54e9f25a0673effe165
Miles-Alford <miles.alford@hackney.gov.uk>
2021-08-24 20:59:50 +0100

    Add tests for patch & get usecase methods

--- ProjectFinderApi.Tests/V1/UseCase/ProjectsUseCaseTests.cs
+++ ProjectFinderApi.Tests/V1/UseCase/ProjectsUseCaseTests.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
+using System.Linq;
+using AutoFixture;
 using FluentAssertions;
 using Moq;
 using NUnit.Framework;
 using ProjectFinderApi.Tests.V1.Helpers;
 using ProjectFinderApi.V1.Boundary.Request;
+using ProjectFinderApi.V1.Boundary.Response;
 using ProjectFinderApi.V1.Factories;
 using ProjectFinderApi.V1.Gateways.Interfaces;
 using ProjectFinderApi.V1.UseCase;
@@ -69,6 +73,52 @@ namespace ProjectFinderApi.Tests.V1.UseCase
 
             response.Should().BeEquivalentTo(project.ToDomain().ToResponse());
 
+        }
+
+        [Test]
+        public void ExecuteGetCallsProjectsGateway()
+        {
+            var projectRequest = TestHelpers.GetProjectRequest();
+
+            var response = _projectsUseCase.ExecuteGet(projectRequest);
+
+            _mockProjectsGateway.Verify(x => x.GetProjectById(projectRequest));
+            _mockProjectsGateway.Verify(x => x.GetProjectById(It.Is<GetProjectRequest>(u => u == projectRequest)), Times.Once());
+        }
+
+        [Test]
+        public void ExecuteGetReturnsAProject()
+        {
+            var projectRequest = TestHelpers.GetProjectRequest();
+            var project = TestHelpers.CreateProject(projectRequest.Id);
+
+            _mockProjectsGateway.Setup(x => x.GetProjectById(projectRequest)).Returns(project);
+
+            var response = _projectsUseCase.ExecuteGet(projectRequest);
+
+            response.Should().BeEquivalentTo(project.ToDomain().ToResponse());
+        }
+
+        [Test]
+        public void ExecuteGetReturnsNullIfNoProjectIsFound()
+        {
+            var projectRequest = TestHelpers.GetProjectRequest();
+
+            var response = _projectsUseCase.ExecuteGet(projectRequest);
+
+            response.Should().BeNull();
+        }
+
+        [Test]
+        public void ExecutePatchCallsProjectsGateway()
+        {
+            var projectRequest = TestHelpers.UpdateProjectRequest();
+            _mockProjectsGateway.Setup(x => x.UpdateProject(projectRequest));
+
+            _projectsUseCase.ExecutePatch(projectRequest);
+
+            _mockProjectsGateway.Verify(x => x.UpdateProject(projectRequest));
+            _mockProjectsGateway.Verify(x => x.UpdateProject(It.Is<UpdateProjectRequest>(u => u == projectRequest)), Times.Once());
         }
     }
 
