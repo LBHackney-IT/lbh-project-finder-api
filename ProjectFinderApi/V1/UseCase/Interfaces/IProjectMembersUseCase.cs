1bca66f3bca5fd01efd45f4f0ff74cc1fed98715
Miles-Alford <miles.alford@hackney.gov.uk>
2021-08-27 19:00:40 +0100

    Add usecase for post endpoint

---
+++ ProjectFinderApi/V1/UseCase/Interfaces/IProjectMembersUseCase.cs
@@ -0,0 +1,11 @@
+using ProjectFinderApi.V1.Boundary.Request;
+using ProjectFinderApi.V1.Boundary.Response;
+
+namespace ProjectFinderApi.V1.UseCase.Interfaces
+{
+    public interface IProjectMembersUseCase
+    {
+        void ExecutePost(CreateProjectMemberRequest request);
+
+    }
+}
