321af3955184d8ce11d2fab019eebaa3690a007d
Miles-Alford <miles.alford@hackney.gov.uk>
2021-09-01 15:49:35 +0100

    update controller

--- ProjectFinderApi/V1/Controllers/ProjectMemberController.cs
+++ ProjectFinderApi/V1/Controllers/ProjectMemberController.cs
@@ -4,11 +4,12 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ProjectFinderApi.V1.Exceptions;
 using ProjectFinderApi.V1.Boundary.Request;
+using System.Collections.Generic;
 
 namespace ProjectFinderApi.V1.Controllers
 {
     [ApiController]
-    [Route("api/v1/members")]
+    [Route("api/v1")]
     [Produces("application/json")]
     [ApiVersion("1.0")]
     public class ProjectMemberController : BaseController
@@ -27,6 +28,7 @@ namespace ProjectFinderApi.V1.Controllers
         /// <response code="422">Could not process request</response>
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [HttpPost]
+        [Route("members")]
         public IActionResult CreateProjectMember([FromBody] CreateProjectMemberRequest request)
         {
             var validator = new CreateProjectMemberRequestValidator();
